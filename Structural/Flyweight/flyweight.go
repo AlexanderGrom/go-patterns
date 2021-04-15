6db54d9ce314e1eca8fd47766e0315179a3c24b8
AlexanderGrom <i2010.grom@yandex.ru>
2021-04-15 20:42:22 +0300

    new flyweight example

--- Structural/Flyweight/flyweight.go
+++ Structural/Flyweight/flyweight.go
@@ -1,41 +1,36 @@
 // Package flyweight is an example of the Flyweight Pattern.
 package flyweight
 
+import "fmt"
+
 // Flyweighter interface
 type Flyweighter interface {
-	GetName() string
-	SetName(name string)
+	Draw(width, height int, opacity float64) string
 }
 
 // FlyweightFactory implements a factory.
 // If a suitable flyweighter is in pool, then returns it.
 type FlyweightFactory struct {
-	pool map[int]Flyweighter
+	pool map[string]Flyweighter
 }
 
 // GetFlyweight creates or returns a suitable Flyweighter by state.
-func (f *FlyweightFactory) GetFlyweight(state int) Flyweighter {
+func (f *FlyweightFactory) GetFlyweight(filename string) Flyweighter {
 	if f.pool == nil {
-		f.pool = make(map[int]Flyweighter)
+		f.pool = make(map[string]Flyweighter)
 	}
-	if _, ok := f.pool[state]; !ok {
-		f.pool[state] = &ConcreteFlyweight{state: state}
+	if _, ok := f.pool[filename]; !ok {
+		f.pool[filename] = &ConcreteFlyweight{filename: filename}
 	}
-	return f.pool[state]
+	return f.pool[filename]
 }
 
 // ConcreteFlyweight implements a Flyweighter interface.
 type ConcreteFlyweight struct {
-	state int
-	name  string
+	filename string // internal state
 }
 
-// GetName returns name
-func (f *ConcreteFlyweight) GetName() string {
-	return "My name: " + f.name
-}
-
-// SetName sets a name
-func (f *ConcreteFlyweight) SetName(name string) {
-	f.name = name
+// Draw draws image. Args width, height and opacity is external state.
+func (f *ConcreteFlyweight) Draw(width, height int, opacity float64) string {
+	return fmt.Sprintf("draw image: %s, width: %d, height: %d, opacity: %.2f", f.filename, width, height, opacity)
 }
--- Structural/Flyweight/flyweight_test.go
+++ Structural/Flyweight/flyweight_test.go
@@ -1,28 +1,32 @@
 package flyweight
 
 import (
+	"strconv"
 	"testing"
 )
 
 func TestFlyweight(t *testing.T) {
+	var testCases = []struct {
+		filename string
+		width    int
+		height   int
+		opacity  float64
+		expect   string
+	}{
+		{"cat.jpg", 100, 100, 0.95, "draw image: cat.jpg, width: 100, height: 100, opacity: 0.95"},
+		{"cat.jpg", 200, 200, 0.75, "draw image: cat.jpg, width: 200, height: 200, opacity: 0.75"},
+		{"dog.jpg", 300, 300, 0.50, "draw image: dog.jpg, width: 300, height: 300, opacity: 0.50"},
+	}
 
-	expect := "My name: Jeck"
+	var factory = new(FlyweightFactory)
 
-	factory := new(FlyweightFactory)
-
-	flyweight1 := factory.GetFlyweight(1)
-	flyweight2 := factory.GetFlyweight(2)
-	flyweight3 := factory.GetFlyweight(3)
-
-	flyweight1.SetName("Jim")
-	flyweight2.SetName("Jeck")
-	flyweight3.SetName("Jill")
-
-	flyweightN := factory.GetFlyweight(2)
-
-	result := flyweightN.GetName()
-
-	if result != expect {
-		t.Errorf("Expect result to equal %s, but %s.\n", expect, result)
+	for i, tt := range testCases {
+		t.Run("case "+strconv.Itoa(i), func(t *testing.T) {
+			var flyweight = factory.GetFlyweight(tt.filename)
+			var result = flyweight.Draw(tt.width, tt.height, tt.opacity)
+			if result != tt.expect {
+				t.Errorf("Expect result to equal %s, but %s.\n", tt.expect, result)
+			}
+		})
 	}
 }
